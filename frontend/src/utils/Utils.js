08052dda549b16b88e4d07ec01df86778392a3c7
Amri Fikri Akbar <110604652+killbaz@users.noreply.github.com>
2025-06-03 02:18:10 +0700

    Rename utils.js to Utils.js

---
+++ frontend/src/utils/Utils.js
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://projectakhir-backend-85236031321.us-central1.run.app";
--- frontend/src/utils/utils.js
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = "https://projectakhir-backend-85236031321.us-central1.run.app";
